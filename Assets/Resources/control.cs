61fdd564751c21cf9f251491af7899306e9320c5
Avekeez <s-ssoetomo@lwsd.org>
2015-10-01 19:08:18 -0700

    photon is hard

---
+++ Assets/Resources/control.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3 (Input.GetAxis ("Horizontal")*0.1f, 0, 0);
+	}
+}
